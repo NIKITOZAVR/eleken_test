e59632eefc935253a50147277bbfc5325b95dd8a
nikitozavr <nikitozavr@email.ua>
2017-03-07 19:48:31 +0200

    added test job

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+
+  $('.header__backet-s-ico').click(function() {
+    $('.header__search-field div').toggleClass('visible');
+    // $('.header__search-field input').toggleClass('visible');
+
+
+      //   $( "#book" ).animate({
+      //   opacity: 0.25,
+      //   left: "+=50",
+      //   height: "toggle"
+      // }, 5000, function() {
+      //   // Animation complete.
+      // });
+  });
+
+
+});
